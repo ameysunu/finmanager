66560a2d7a67e295f93ea775e4c7af317e852d18
Amey Sunu <ameysunu00719@gmail.com>
2020-10-17 15:20:21 +0300

    home added

--- lib/home.dart
+++ lib/home.dart
@@ -35,34 +35,223 @@ class _HomeState extends State<Home> {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
+                  padding: const EdgeInsets.only(top: 50.0),
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Padding(
-                            padding: const EdgeInsets.only(right: 20.0),
+                            padding:
+                                const EdgeInsets.only(right: 10.0, bottom: 20),
                             child: Container(
-                              height: 175,
-                              width: 150,
+                              height: 250,
+                              width: 175,
                               child: Card(
+                                color: HexColor('#2E2C2C'),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                ),
                                 child: Column(
-                                  children: [],
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Center(
+                                        child: Container(
+                                            child: Icon(
+                                      Icons.compare_arrows_rounded,
+                                      color: HexColor('#ED5E93'),
+                                      size: 40,
+                                    ))),
+                                    Padding(
+                                      padding: const EdgeInsets.all(10.0),
+                                      child: Text(
+                                        "Split\nChecker",
+                                        style: TextStyle(
+                                            fontFamily: 'Metropolis',
+                                            color: Colors.white,
+                                            fontSize: 20),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(10.0),
+                                      child: Text(
+                                        "Split with friends easily.",
+                                        style: TextStyle(
+                                            fontFamily: 'Metropolis',
+                                            color: Colors.white54,
+                                            fontSize: 15),
+                                      ),
+                                    )
+                                  ],
                                 ),
                               ),
                             ),
                           ),
                           Padding(
-                            padding: const EdgeInsets.only(left: 20.0),
+                            padding:
+                                const EdgeInsets.only(left: 10.0, bottom: 20),
                             child: Container(
-                              height: 175,
-                              width: 150,
+                              height: 250,
+                              width: 175,
                               child: Card(
+                                elevation: 5,
+                                color: HexColor('#2E2C2C'),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                ),
                                 child: Column(
-                                  children: [],
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Center(
+                                        child: Container(
+                                            child: Icon(
+                                      Icons.attach_money,
+                                      color: HexColor('#00E6E4'),
+                                      size: 40,
+                                    ))),
+                                    Padding(
+                                      padding: const EdgeInsets.all(10.0),
+                                      child: Text(
+                                        "Your\nCards",
+                                        style: TextStyle(
+                                            fontFamily: 'Metropolis',
+                                            color: Colors.white,
+                                            fontSize: 20),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(10.0),
+                                      child: Text(
+                                        "Monitor your card usage.",
+                                        style: TextStyle(
+                                            fontFamily: 'Metropolis',
+                                            color: Colors.white54,
+                                            fontSize: 15),
+                                      ),
+                                    )
+                                  ],
                                 ),
                               ),
+                              // decoration: new BoxDecoration(
+                              //   boxShadow: [
+                              //     new BoxShadow(
+                              //       color: Colors.white38,
+                              //       blurRadius: 20.0,
+                              //     ),
+                              //   ],
+                              // ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(right: 10.0, bottom: 20),
+                            child: Container(
+                              height: 250,
+                              width: 175,
+                              child: Card(
+                                color: HexColor('#2E2C2C'),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Center(
+                                        child: Container(
+                                            child: Icon(
+                                      Icons.comment_bank,
+                                      color: HexColor('#FF683E'),
+                                      size: 40,
+                                    ))),
+                                    Padding(
+                                      padding: const EdgeInsets.all(10.0),
+                                      child: Text(
+                                        "Currency\nConverter",
+                                        style: TextStyle(
+                                            fontFamily: 'Metropolis',
+                                            color: Colors.white,
+                                            fontSize: 20),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(10.0),
+                                      child: Text(
+                                        "Get live currency rates.",
+                                        style: TextStyle(
+                                            fontFamily: 'Metropolis',
+                                            color: Colors.white54,
+                                            fontSize: 15),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 10.0, bottom: 20),
+                            child: Container(
+                              height: 250,
+                              width: 175,
+                              child: Card(
+                                elevation: 5,
+                                color: HexColor('#2E2C2C'),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Center(
+                                        child: Container(
+                                            child: Icon(
+                                      Icons.info_outline_rounded,
+                                      color: HexColor('#B779FF'),
+                                      size: 40,
+                                    ))),
+                                    Padding(
+                                      padding: const EdgeInsets.all(10.0),
+                                      child: Text(
+                                        "User\nDetails",
+                                        style: TextStyle(
+                                            fontFamily: 'Metropolis',
+                                            color: Colors.white,
+                                            fontSize: 20),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(10.0),
+                                      child: Text(
+                                        "Monitor your card usage.",
+                                        style: TextStyle(
+                                            fontFamily: 'Metropolis',
+                                            color: Colors.white54,
+                                            fontSize: 15),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              // decoration: new BoxDecoration(
+                              //   boxShadow: [
+                              //     new BoxShadow(
+                              //       color: Colors.white38,
+                              //       blurRadius: 20.0,
+                              //     ),
+                              //   ],
+                              // ),
                             ),
                           ),
                         ],
