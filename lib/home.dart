ac96f2679d5df5f7d05d33d554bc97392dc6da77
Amey Sunu <ameysunu00719@gmail.com>
2020-10-17 14:12:33 +0300

    home added

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
+import 'package:finmanager/login.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,7 +11,34 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(20.0, 30, 10, 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "18th October 2020",
+                  style: TextStyle(
+                      color: HexColor('#CBCCFF'), fontFamily: 'Metropolis'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    "Hello, $name",
+                    style: TextStyle(
+                        color: HexColor('#FFFFFF'),
+                        fontFamily: 'Metropolis',
+                        fontSize: 25),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
