2d74babb101fb121a6e7ed1c09e00ecc540f1c89
Amey Sunu <ameysunu00719@gmail.com>
2020-10-18 11:33:51 +0300

    split added

--- lib/pages/split.dart
+++ lib/pages/split.dart
@@ -1,4 +1,13 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final billController = TextEditingController();
+final tipController = TextEditingController();
+
+int calc = 0;
+int counter = 0;
 
 class Split extends StatefulWidget {
   @override
@@ -6,6 +15,24 @@ class Split extends StatefulWidget {
 }
 
 class _SplitState extends State<Split> {
+  void billCalc() {
+    setState(() => calc);
+    var bill = double.parse(billController.text);
+    calc = bill ~/ counter;
+  }
+
+  void increment() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +44,7 @@ class _SplitState extends State<Split> {
         child: SingleChildScrollView(
           child: Container(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Padding(
                   padding: const EdgeInsets.all(10.0),
@@ -28,7 +56,163 @@ class _SplitState extends State<Split> {
                         fontSize: 25),
                   ),
                 ),
-                Container(),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    color: HexColor('#2E2C2C'),
+                    height: MediaQuery.of(context).size.height * 0.5,
+                    width: MediaQuery.of(context).size.width * 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 10.0, right: 10, top: 10),
+                          child: Text(
+                            "Enter total bill",
+                            style: TextStyle(
+                                fontFamily: 'Metropolis', color: Colors.white),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: TextField(
+                            controller: billController,
+                            decoration: new InputDecoration(
+                                hintText: 'eg. 123.56',
+                                hintStyle: TextStyle(
+                                    fontFamily: 'Metropolis',
+                                    color: Colors.white54,
+                                    fontSize: 30)),
+                            textAlignVertical: TextAlignVertical.center,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontFamily: 'Metropolis'),
+                            keyboardType:
+                                TextInputType.numberWithOptions(decimal: true),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 10.0, right: 10, top: 10),
+                              child: Text(
+                                "Choose Split",
+                                style: TextStyle(
+                                  fontFamily: 'Metropolis',
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 10.0, right: 10, top: 10),
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.remove,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {
+                                  decrement();
+                                },
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 10.0, right: 10, top: 10),
+                              child: Text(
+                                counter.toString(),
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'Metropolis',
+                                    fontSize: 20),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 10.0, right: 10, top: 10),
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.add,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {
+                                  increment();
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 10.0, right: 10, top: 10),
+                              child: Text(
+                                "Enter Tip",
+                                style: TextStyle(
+                                  fontFamily: 'Metropolis',
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 10.0, right: 10, top: 10),
+                              child: Container(
+                                width: 100,
+                                child: TextField(
+                                  controller: tipController,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontFamily: 'Metropolis'),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Center(
+                            child: Text(
+                          calc.toString(),
+                          style: TextStyle(color: Colors.white),
+                        )),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10.0, 10, 10, 10),
+                          child: RaisedButton(
+                            color: HexColor('#ED5E93'),
+                            child: Container(
+                              height: 50,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "Calculate",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: "Metropolis",
+                                        fontSize: 18),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            onPressed: () {
+                              billCalc();
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
@@ -37,3 +221,5 @@ class _SplitState extends State<Split> {
     );
   }
 }
+
+void popup() {}
